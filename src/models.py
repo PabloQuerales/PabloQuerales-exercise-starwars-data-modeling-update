4bfe18d2dabc5855fc1853300afe4f30fd010cd0
Pablo Querales <119375308+PabloQuerales@users.noreply.github.com>
2025-02-10 12:32:53 +0000

    proyecto terminado

--- src/models.py
+++ src/models.py
@@ -1,29 +1,53 @@
 import os
 import sys
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column
-from sqlalchemy import create_engine
+from typing import List
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
+from sqlalchemy import create_engine, ForeignKey
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Users(Base):
+    __tablename__ = 'users'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id: Mapped[int] = mapped_column(primary_key=True)
+    password: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    last_name: Mapped[str] = mapped_column(nullable=False)
+    favorites: Mapped[List["Favorites"]] = relationship()
+
+class Planets(Base):
+    __tablename__ = 'planets'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
+    climate: Mapped[str] = mapped_column(nullable=False)
+    diameter: Mapped[str] = mapped_column(nullable=False)
+    gravity: Mapped[str] = mapped_column(nullable=False)
+    favorites: Mapped[List["Favorites"]] = relationship()
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+class Persons(Base):
+    __tablename__ = 'persons'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(nullable=False)
+    eyes_color: Mapped[str] = mapped_column(nullable=False)
+    hair_color: Mapped[str] = mapped_column(nullable=False)
+    heigth: Mapped[str] = mapped_column(nullable=False)
+    favorites: Mapped[List["Favorites"]] = relationship()
 
-    def to_dict(self):
-        return {}
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    person_id: Mapped[int] = mapped_column(ForeignKey("persons.id"))
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planets.id"))
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
